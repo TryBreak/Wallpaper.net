83df78704c49c1b0ab611c496a966744e5d35cf1
墨七 <meichangliang@mo7.cc>
2022-05-31 04:43:29 +0800

    exit-push

--- _shell/build.sh
+++ _shell/build.sh
@@ -3,17 +3,11 @@
 source "./_shell/init.sh"
 #############
 
-echo " =========== 正在进行编译 aarch64 =========== "
-
-go mod tidy &&
-  GOOS=linux GOARCH=arm64 GOARM=7 go build -o ${buildName}"_aarch64" -tags=jsoniter . &&
-  echo "编译 完成"
-
-echo " =========== 正在进行编译 x86_64 =========== "
+echo " =========== 正在进行编译 =========== "
 
 set GOARCH=amd64
 go mod tidy &&
-  GOOS=linux GOARCH=amd64 go build -o ${buildName}"_x86_64" -tags=jsoniter . &&
+  GOOS=linux GOARCH=amd64 go build -o ${buildName} -tags=jsoniter . &&
   echo "编译 完成"
 
 echo " =========== 开始进行 文件整理 =========== "
@@ -22,6 +16,5 @@ echo "清理并创建 dist 目录"
 rm -rf ${outPutPath}
 mkdir ${outPutPath} &&
   echo "移动 goRun 文件"
-mv ${buildName}"_aarch64" ${outPutPath} &&
-  mv ${buildName}"_x86_64" ${outPutPath} &&
+mv ${buildName} ${outPutPath} &&
   exit
