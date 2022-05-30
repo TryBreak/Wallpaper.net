22aeb484852a39f5042fd5e518157f1e8b1db1be
墨七 <meichangliang@mo7.cc>
2022-05-31 02:43:22 +0800

    exit-push

--- router/public/router.go
+++ router/public/router.go
@@ -12,10 +12,6 @@ func Router(router *gin.RouterGroup) {
 	router.GET("", middleWare.Index(" /api/public 接口首页 "))
 	router.GET("/", middleWare.Index(" /api/public 接口首页 "))
 
-	router.POST("/shell_run", RunShell)
-
-	router.POST("/github_webhooks", GitHun)
-
 	router.GET("/ping", middleWare.GetPing)
 	router.POST("/ping", middleWare.PostPing)
 }
